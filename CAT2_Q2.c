3ff0c255350a4b446159c331f0c6996fff8ca965
Shem <sammyshem15@gmail.com>
2025-11-03 18:19:59 +0300

    Add files via upload

---
+++ CAT2_Q2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    float hours_worked, hourly_wage;
+    float gross_pay, taxes, net_pay;
+    float overtime_hours;
+
+    printf("Enter hours worked in a week: ");
+    scanf("%f", &hours_worked);
+
+    printf("Enter hourly wage: ");
+    scanf("%f", &hourly_wage);
+
+    if (hours_worked > 40) {
+        overtime_hours = hours_worked - 40;
+        gross_pay = (40 * hourly_wage) + (overtime_hours * hourly_wage * 1.5);
+    } else {
+        gross_pay = hours_worked * hourly_wage;
+    }
+
+    if (gross_pay <= 600) {
+        taxes = 0.15 * gross_pay;
+    } else {
+        taxes = (0.15 * 600) + (0.20 * (gross_pay - 600));
+    }
+
+    net_pay = gross_pay - taxes;
+
+    printf("\nGross Pay: $%.2f\n", gross_pay);
+    printf("Taxes: $%.2f\n", taxes);
+    printf("Net Pay: $%.2f\n", net_pay);
+
+    return 0;
+}
---
+++ CAT2_q3.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+void writeInputFile() {
+    FILE *fp;
+    int numbers[SIZE];
+
+    fp = fopen("input.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening input file!\n");
+        exit(1);
+    }
+
+    printf("Enter %d integers:\n", SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        scanf("%d", &numbers[i]);
+        fprintf(fp, "%d\n", numbers[i]);
+    }
+
+    fclose(fp);
+}
+
+void processFile() {
+    FILE *inFile, *outFile;
+    int num, sum = 0, count = 0;
+    float avg;
+
+    inFile = fopen("input.txt", "r");
+    if (inFile == NULL) {
+        printf("Error opening input.txt!\n");
+        exit(1);
+    }
+
+    outFile = fopen("output.txt", "w");
+    if (outFile == NULL) {
+        printf("Error opening output.txt!\n");
+        fclose(inFile);
+        exit(1);
+    }
+
+    while (fscanf(inFile, "%d", &num) == 1) {
+        sum += num;
+        count++;
+    }
+
+    avg = (float)sum / count;
+
+    fprintf(outFile, "Sum = %d\n", sum);
+    fprintf(outFile, "Average = %.2f\n", avg);
+
+    fclose(inFile);
+    fclose(outFile);
+}
+
+void displayFiles() {
+    FILE *fp;
+    char ch;
+
+    printf("\nContents of input.txt:\n");
+    fp = fopen("input.txt", "r");
+    if (fp == NULL) {
+        printf("Error reading input.txt!\n");
+        return;
+    }
+    while ((ch = fgetc(fp)) != EOF)
+        putchar(ch);
+    fclose(fp);
+
+    printf("\nContents of output.txt:\n");
+    fp = fopen("output.txt", "r");
+    if (fp == NULL) {
+        printf("Error reading output.txt!\n");
+        return;
+    }
+    while ((ch = fgetc(fp)) != EOF)
+        putchar(ch);
+    fclose(fp);
+}
+
+int main() {
+    writeInputFile();   
+    processFile();      
+    displayFiles();     
+
+    return 0;
+}
